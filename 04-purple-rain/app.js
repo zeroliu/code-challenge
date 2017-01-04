28e72c583e3555fd09d3cba5d3e9472a1c784052
Zero Liu <zerolxy@gmail.com>
2017-01-03 23:05:27 -0800

    update purple rain

--- 04-purple-rain/app.js
+++ 04-purple-rain/app.js
@@ -1,16 +1,16 @@
-const p5 = require('p5');
 const Drop = require('./drop');
 
 new p5((s) => {
-  const WIDTH = 640;
-  const HEIGHT = 480;
-  const DROP_NUM = 500;
+  let canvasWidth, canvasHeight;
+  const DROP_NUM = 300;
   const drops = [];
 
   const setup = () => {
-    s.createCanvas(WIDTH, HEIGHT);
+    canvasWidth = s.displayWidth * 0.95;
+    canvasHeight = canvasWidth;
+    s.createCanvas(canvasWidth, canvasHeight);
     for (let i = 0; i< DROP_NUM; i++) {
-      drops.push(new Drop(WIDTH, HEIGHT, s));
+      drops.push(new Drop(canvasWidth, canvasHeight, s));
     }
   };
 

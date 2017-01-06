59d0f19f94b541a8d12405713c8312dd184f9bb8
zeroliu <zerolxy@gmail.com>
2017-01-06 11:20:29 -0800

    add interaction to purple rain

--- purple-rain/app.js
+++ purple-rain/app.js
@@ -2,19 +2,32 @@ const Drop = require('./drop');
 
 new p5((s) => {
   let canvasWidth, canvasHeight;
-  const DROP_NUM = 300;
+  const MAX_DROP_NUM = 300;
+  const MIN_DROP_NUM = 50;
   const drops = [];
 
   const setup = () => {
     canvasWidth = s.displayWidth * 0.95;
     canvasHeight = canvasWidth;
     s.createCanvas(canvasWidth, canvasHeight);
-    for (let i = 0; i< DROP_NUM; i++) {
+    for (let i = 0; i< s.random(MIN_DROP_NUM, MAX_DROP_NUM); i++) {
       drops.push(new Drop(canvasWidth, canvasHeight, s));
     }
   };
 
   const draw = () => {
+    if (s.touches.length > 0) {
+      const dropNum = s.map(
+        s.touches[0].x, 0, canvasWidth, MIN_DROP_NUM, MAX_DROP_NUM
+      );
+      if (dropNum > drops.length) {
+        for (let i = 0; i< dropNum - drops.length; i++) {
+          drops.push(new Drop(canvasWidth, canvasHeight, s));
+        }
+      } else {
+        drops.splice(dropNum);
+      }
+    }
     s.background(230, 230, 250);
     for (let drop of drops) {
       drop.update();
